088b40ea00cc409d92740017fd01c1b0b124c839
Julio C. Rocha <julio@rochsquadron.net>
2018-07-08 15:09:52 -0700

    Added Source and Target parameters to Update-Projects script. Imported Settings project.

--- Source/Scripts/Update-Projects.ps1
+++ Source/Scripts/Update-Projects.ps1
@@ -4,6 +4,12 @@
 # Updates the sources and headers based in the Project64 code base.
 #
 param (
+	[Parameter(Mandatory=$true)]
+	[string] $Source,
+	[Parameter(Mandatory=$true)]
+	[string] $Target,
+	[string] $ItemPath,
+	[string]
 	$MSBuildHome = "${env:ProgramFiles(x86)}\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0"
 )
 
@@ -16,18 +22,22 @@ Add-Type -Path "$MSBuildHome\Bin\Microsoft.Build.dll"
 # Default options
 $options = $options = [Microsoft.Build.Definition.ProjectOptions]::new()
 
-$projectPath = (Get-ChildItem .\modules\project64\Source\Common\Common.vcxproj).FullName
-$sourceProject = [Microsoft.Build.Evaluation.Project]::FromFile($projectPath, $options)
+#$projectPath = (Get-ChildItem .\modules\project64\Source\Common\Common.vcxproj).FullName
+$sourceProject = [Microsoft.Build.Evaluation.Project]::FromFile((Get-ChildItem $Source).FullName, $options)
 $headers = $sourceProject.Items | Where-Object { $_.ItemType -eq 'ClInclude' }
 $sources = $sourceProject.Items | Where-Object { $_.ItemType -eq 'ClCompile' }
 
-$projectPath = (Get-ChildItem .\Source\MSBuild\Common.vcxproj).FullName
-$targetProject = [Microsoft.Build.Evaluation.Project]::FromFile($projectPath, $options)
+#$projectPath = (Get-ChildItem .\Source\MSBuild\Common.vcxproj).FullName
+$targetProject = [Microsoft.Build.Evaluation.Project]::FromFile((Get-ChildItem $Target).FullName, $options)
+
+# Clear items.
+$oldItems = $targetProject.Items | Where-Object { $_.ItemType -in ('ClInclude', 'ClCompile') }
+$oldItems | ForEach-Object { $targetProject.RemoveItem($_) }
 
 # foreach ($header in $headers) {
 # 	$targetProject.AddItem($header.ItemType, "`$(SourceRoot)Common\$($header.UnevaluatedInclude)")
 # }
-$headers | ForEach-Object { $targetProject.AddItem($_.ItemType, "`$(SourceRoot)Common\$($_.UnevaluatedInclude)") }
-$sources | ForEach-Object { $targetProject.AddItem($_.ItemType, "`$(SourceRoot)Common\$($_.UnevaluatedInclude)") }
+$headers | ForEach-Object { $targetProject.AddItem($_.ItemType, "`$(SourceRoot)$ItemPath\$($_.UnevaluatedInclude)") }
+$sources | ForEach-Object { $targetProject.AddItem($_.ItemType, "`$(SourceRoot)$ItemPath\$($_.UnevaluatedInclude)") }
 
 $targetProject.Save()
