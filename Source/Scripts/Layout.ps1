2635b0e063fcba55d9a8f5ad0da60160ddc14746
Julio C. Rocha <julio@rochsquadron.net>
2018-07-21 02:01:08 -0700

    Add InstallDir parameter to Layout.ps1.

--- Source/Scripts/Layout.ps1
+++ Source/Scripts/Layout.ps1
@@ -7,20 +7,23 @@ param (
 	[ValidateSet('Win32')]
 	[string] $Platform = 'Win32',
 	[ValidateSet('Debug', 'Release')]
-	[string] $Configuration = 'Release'
+	[string] $Configuration = 'Release',
+	[string] $InstallDir = "$($PSScriptRoot | Split-Path | Split-Path)\Target\$Platform\$Configuration\Project64"
 )
 
 $root = $PSScriptRoot | Split-Path | Split-Path
 $pj64Root = "$root\modules\project64"
 $targetRoot = "$root\Target\$Platform\$Configuration"
-$installDir = "$targetRoot\Project64"
 
-('Lang', 'Config') | ForEach-Object { Copy-Item -Recurse -Force $pj64Root\$_ $installDir\ }
-('Audio', 'GFX', 'Input', 'RSP') | ForEach-Object { New-Item -ItemType Directory -Force $installDir\Plugin\$_ }
-Copy-Item -Recurse -Force $root\Source\Config $installDir
+# Ensure install paths exists
+New-Item -ItemType Directory -Force $InstallDir | Out-Null
+('Audio', 'GFX', 'Input', 'RSP') | ForEach-Object { New-Item -ItemType Directory -Force $InstallDir\Plugin\$_ | Out-Null }
+('Lang', 'Config') | ForEach-Object { Copy-Item -Recurse -Force $pj64Root\$_ $InstallDir\ }
+Copy-Item -Recurse -Force $root\Source\Config $InstallDir
 
-# Copy plugins
-Copy-Item $targetRoot\Project64-audio\Project64-audio.dll $installDir\Plugin\Audio\
-Copy-Item $targetRoot\Project64-input\Project64-input.dll $installDir\Plugin\Input\
-Copy-Item $targetRoot\Project64-video\Project64-video.dll $installDir\Plugin\GFX\
-Copy-Item $targetRoot\RSP\RSP.dll $installDir\Plugin\RSP\
+# Copy binaries
+Copy-Item $targetRoot\Project64\Project64.exe $InstallDir\ -ErrorAction Ignore
+Copy-Item $targetRoot\Project64-audio\Project64-audio.dll $InstallDir\Plugin\Audio\
+Copy-Item $targetRoot\Project64-input\Project64-input.dll $InstallDir\Plugin\Input\
+Copy-Item $targetRoot\Project64-video\Project64-video.dll $InstallDir\Plugin\GFX\
+Copy-Item $targetRoot\RSP\RSP.dll $InstallDir\Plugin\RSP\
