714f46a973fd8f427ea1d230c33d2a405c7dac54
Julio C. Rocha <julio@rochsquadron.net>
2018-07-22 03:39:21 -0700

    Set version string as MSBuild property.

--- Source/Cpp/Project64-core/Version.h
+++ Source/Cpp/Project64-core/Version.h
@@ -40,12 +40,12 @@
 #endif //GIT_VERSION
 
 #define VER_FILE_DESCRIPTION_STR    "Project64"
-#define VER_FILE_VERSION            VERSION_MAJOR, VERSION_MINOR, VERSION_REVISION, VERSION_BUILD
+#define VER_FILE_VERSION            VERSION_MAJOR, VERSION_MINOR, VERSION_BUILD, VERSION_REVISION
 #define VER_FILE_VERSION_STR        STRINGIZE(VERSION_MAJOR)        \
                                     "." STRINGIZE(VERSION_MINOR)    \
-                                    "." STRINGIZE(VERSION_REVISION) \
                                     "." STRINGIZE(VERSION_BUILD)    \
-                                    "-" GIT_VERSION
+                                    "." STRINGIZE(VERSION_REVISION)
+//                                    "-" GIT_VERSION
 
 #define VER_PRODUCTNAME_STR         "Project64"
 #define VER_PRODUCT_VERSION         VER_FILE_VERSION
