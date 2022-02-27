33d4b1ab38b37effcd8293abf97a87666d0f0a74
Julio C. Rocha <dev@rocha.red>
2022-02-27 05:01:54 -0800

    Clean up WinApiStubs

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -6,44 +6,11 @@
 
 #include <iostream>
 
-//#pragma comment(lib, "Wininet.lib")
-//#pragma comment(lib, "StubWinInet.lib")
-//#pragma comment(lib, "WinApiStubs.lib")
-
-//namespace {
-//
-//	char sessionStubChars[1];
-//	LPVOID sessionStubVoid = static_cast<LPVOID>(sessionStubChars); //(LPVOID)lpb;
-//
-//} //namespace
-//
-//EXTERN_C /*__declspec(dllexport)*/ HINTERNET STDAPICALLTYPE InternetOpenW(
-//	_In_opt_ LPCWSTR lpszAgent,
-//	_In_ DWORD dwAccessType,
-//	_In_opt_ LPCWSTR lpszProxy,
-//	_In_opt_ LPCWSTR lpszProxyBypass,
-//	_In_ DWORD dwFlags
-//)
-//{
-//	return sessionStubVoid;
-//}
-
+#pragma comment(lib, "Wininet.lib")
 
 int main()
 {
-	//HINTERNET hSession = InternetOpen(L"Project64", INTERNET_OPEN_TYPE_PRECONFIG, nullptr, nullptr, 0);
-	HINTERNET hSession = InternetOpenW(L"Project64", INTERNET_OPEN_TYPE_PRECONFIG, nullptr, nullptr, 0);
+	HINTERNET hSession = InternetOpen(L"Project64", INTERNET_OPEN_TYPE_PRECONFIG, nullptr, nullptr, 0);
 
     std::cout << "Hello World!\n";
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
--- WinApiStubs/WinApiStubs.cpp
+++ WinApiStubs/WinApiStubs.cpp
@@ -1,33 +1,15 @@
 // WinApiStubs.cpp : Defines the exported functions for the DLL.
 //
 
-#include "pch.h"
-#include "framework.h"
 #include "WinApiStubs.h"
-
-
-// This is an example of an exported variable
-WINAPISTUBS_API int nWinApiStubs=0;
-
-// This is an example of an exported function.
-WINAPISTUBS_API int fnWinApiStubs(void)
-{
-    return 0;
-}
-
-// This is the constructor of a class that has been exported.
-CWinApiStubs::CWinApiStubs()
-{
-    return;
-}
 
 #include <Windows.h>
 #include <WinInet.h>
 
 namespace {
 
-	char sessionStubChars[1];
-	LPVOID sessionStubVoid = static_cast<LPVOID>(sessionStubChars); //(LPVOID)lpb;
+char internetOpenStubChars[1];
+LPVOID internetOpenStubVoid = static_cast<LPVOID>(internetOpenStubChars);
 
 } //namespace
 
@@ -39,5 +21,5 @@ EXTERN_C __declspec(dllexport) HINTERNET STDAPICALLTYPE InternetOpenW(
 	_In_ DWORD dwFlags
 )
 {
-	return sessionStubVoid;
+	return internetOpenStubVoid;
 }
