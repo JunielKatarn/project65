1bf5e6391d67b8c05a50bd478abdf0a9488495c6
Julio C. Rocha <dev@rocha.red>
2022-02-27 06:08:49 -0800

    Add remaining Wininet stubs

--- Source/Cpp/StubWinInet/StubWinInet.cpp
+++ Source/Cpp/StubWinInet/StubWinInet.cpp
@@ -1,25 +1,4 @@
 // StubWinInet.cpp : Defines the functions for the static library.
 //
 
-//#include <Windows.h>
-//#include <WinInet.h>
-//
-//namespace {
-//
-//char sessionStubChars[1];
-//LPVOID sessionStubVoid = static_cast<LPVOID>(sessionStubChars); //(LPVOID)lpb;
-//
-//} //namespace
-//
-///*EXTERN_C*/ /*__declspec(dllexport)*/ HINTERNET STDAPICALLTYPE InternetOpenW(
-//	_In_opt_ LPCWSTR lpszAgent,
-//	_In_ DWORD dwAccessType,
-//	_In_opt_ LPCWSTR lpszProxy,
-//	_In_opt_ LPCWSTR lpszProxyBypass,
-//	_In_ DWORD dwFlags
-//)
-//{
-//	return sessionStubVoid;
-//}
-
 void Foo() {}
--- Source/Cpp/WinApiStubs/WinApiStubs.cpp
+++ Source/Cpp/WinApiStubs/WinApiStubs.cpp
@@ -8,8 +8,8 @@
 
 namespace {
 
-char internetOpenStubChars[1];
-LPVOID internetOpenStubVoid = static_cast<LPVOID>(internetOpenStubChars);
+char charsStub[1];
+LPVOID voidStub = static_cast<LPVOID>(charsStub);
 
 } //namespace
 
@@ -21,5 +21,72 @@ EXTERN_C __declspec(dllexport) HINTERNET STDAPICALLTYPE InternetOpenW(
 	_In_ DWORD dwFlags
 )
 {
-	return internetOpenStubVoid;
+	return voidStub;
+}
+
+EXTERN_C __declspec(dllexport) HINTERNET STDAPICALLTYPE
+InternetConnectW(
+	_In_ HINTERNET hInternet,
+	_In_ LPCWSTR lpszServerName,
+	_In_ INTERNET_PORT nServerPort,
+	_In_opt_ LPCWSTR lpszUserName,
+	_In_opt_ LPCWSTR lpszPassword,
+	_In_ DWORD dwService,
+	_In_ DWORD dwFlags,
+	_In_opt_ DWORD_PTR dwContext
+)
+{
+	return voidStub;
+}
+
+EXTERN_C __declspec(dllexport) HINTERNET STDAPICALLTYPE
+HttpOpenRequestW(
+	_In_ HINTERNET hConnect,
+	_In_opt_ LPCWSTR lpszVerb,
+	_In_opt_ LPCWSTR lpszObjectName,
+	_In_opt_ LPCWSTR lpszVersion,
+	_In_opt_ LPCWSTR lpszReferrer,
+	_In_opt_z_ LPCWSTR FAR* lplpszAcceptTypes,
+	_In_ DWORD dwFlags,
+	_In_opt_ DWORD_PTR dwContext
+)
+{
+	return voidStub;
+}
+
+//BOOLAPI 
+
+//EXTERN_C __declspec(dllexport) BOOL _Success_(return != FALSE) STDAPICALLTYPE
+extern "C" /*__declspec(dllimport)*/ BOOL __stdcall
+InternetCloseHandle(
+	_In_ HINTERNET hInternet
+)
+{
+	return FALSE;
+}
+
+EXTERN_C __declspec(dllexport) BOOL _Success_(return != FALSE) STDAPICALLTYPE
+HttpSendRequestW(
+	_In_ HINTERNET hRequest,
+	_In_reads_opt_(dwHeadersLength) LPCWSTR lpszHeaders,
+	_In_ DWORD dwHeadersLength,
+	_In_reads_bytes_opt_(dwOptionalLength) LPVOID lpOptional,
+	_In_ DWORD dwOptionalLength
+)
+{
+	return FALSE;
+}
+
+EXTERN_C __declspec(dllexport) BOOL _Success_(return != FALSE) STDAPICALLTYPE
+HttpQueryInfoW(
+	_In_ HINTERNET hRequest,
+	_In_ DWORD dwInfoLevel,
+	_Inout_updates_bytes_to_opt_(*lpdwBufferLength, *lpdwBufferLength) __out_data_source(NETWORK) LPVOID lpBuffer,
+	_Inout_ LPDWORD lpdwBufferLength,
+	_Inout_opt_ LPDWORD lpdwIndex
+)
+{
+	*reinterpret_cast<DWORD*>(lpBuffer) = 200;
+
+	return FALSE;
 }
